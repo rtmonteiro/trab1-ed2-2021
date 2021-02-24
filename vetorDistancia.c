a2c5ebf2256ddf1a558c89f3035facb59f1d83f4
AHalic <sophie.gama@aluno.ufes.br>
2021-02-24 17:33:05 -0300

    adiciona struct vetor distancia

---
+++ vetorDistancia.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "vetorDistancia.h"
+
+struct vetorDistancia
+{
+    Ponto* p1;
+    Ponto* p2;
+    double dist;
+};
---
+++ vetorDistancia.h
@@ -0,0 +1,12 @@
+#ifndef VETOR_DIST_H
+    #define VETOR_DIST_H
+
+    // Retorna dist
+    // inializa vetor
+    // insere distancias
+    // ordena vetor
+    
+
+    
+
+#endif
