f850bb3521a85511187e1e11bb48aa40977b8608
bea <beamaia.work@gmail.com>
2021-02-25 12:03:41 -0300

    identa as funções

--- uf.h
+++ uf.h
@@ -1,29 +1,28 @@
 #ifndef UF_H
-#define UF_H
+    #define UF_H
+    #include <stdbool.h>
+    typedef struct uf UF;
 
-#include <stdbool.h>
-typedef struct uf UF;
+    UF* unionFindInit (int N);
 
-UF* unionFindInit (int N);
+    int getID (UF *id, int i);
 
-int getID (UF *id, int i);
+    void putID (UF* id, int i, int j);
 
-void putID (UF* id, int i, int j);
+    int getSize (UF* id, int i);
 
-int getSize (UF* id, int i);
+    void increaseSize (UF* id, int i, int j);
 
-void increaseSize (UF* id, int i, int j);
+    int getRoot (UF *id, int i);
 
-int getRoot (UF *id, int i);
+    bool connected (UF* id, int p, int q);
 
-bool connected (UF* id, int p, int q);
+    void createUnion (UF* id, int p, int q);
 
-void createUnion (UF* id, int p, int q);
+    bool allObjectsConnected (UF* id); 
 
-bool allObjectsConnected (UF* id); 
+    void showUnionFind (UF* id);
 
-void showUnionFind (UF* id);
-
-void freeUnionFind(UF* id);
+    void freeUnionFind(UF* id);
 
 #endif
