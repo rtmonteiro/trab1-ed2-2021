fe8591d710cfbceb97998f5dd2463a0284a8c87f
bea <beamaia.work@gmail.com>
2021-03-03 12:24:36 -0300

    adiciona union-find e valor k

--- main.c
+++ main.c
@@ -5,23 +5,29 @@
 #include "pilha.h"
 #include "distancia.h"
 #include "leArquivo.h"
-
+#include "uf.h"
 
 int main(int argc, char** argv ) {
 
-    char *FILENAME = argv[1];
+    char *FILENAME = argv[2];
 
     FILE *fp = fopen(FILENAME, "r");
     if (!fp) {
         fprintf(stderr, "Error opening file '%s'\n", FILENAME);
-        return EXIT_FAILURE;
+        exit(1);
     }
 
+    int k = atoi(argv[1]);
     Pilha *pilhaPontos = initPilha();
     pilhaPontos = leArquivo(fp, pilhaPontos);
 
     Distancia **vetorDistancias = distanciasPilha(pilhaPontos);
-
+    int tam = getQtd(pilhaPontos);
+    int tamPA = tam * (tam - 1) / 2;
+    UF* unionFind = initUnionFind(tam);
+    mostraUnionFind(unionFind);
+    agrupaCaminhos(unionFind, vetorDistancias, tam, tamPA, k);
+    mostraUnionFind(unionFind);
     liberaPilha(pilhaPontos);
 
     printf("IUPI!");
