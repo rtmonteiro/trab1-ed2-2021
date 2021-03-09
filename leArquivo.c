fb9cd809b935a40dd00e2cd97a595ed4191e0d38
AHalic <sophie.gama@aluno.ufes.br>
2021-03-09 20:02:40 -0300

    Modifica mini mudanças estéticas

--- leArquivo.c
+++ leArquivo.c
@@ -10,21 +10,21 @@
 #define TAM 10
 
 
-static int leM(FILE *fp, PlanoR *p) {
+static int leM(FILE* fp, PlanoR* p) {
 
     //le a primeira linha e retorna m
-    char *line_buf = NULL;
+    char* line_buf = NULL;
     size_t line_buf_size = 0;
 
     int m = 0;
 
     getline(&line_buf, &line_buf_size, fp);
 
-    char *token = strtok(line_buf, ","); /* pega o id do  */
+    char* token = strtok(line_buf, ","); // pega o id do 
 
     while (token) {
         token = strtok(NULL, ",");
-        if (token) m++; /* Ao ler uma nova coordenada, atualiza a dimensão dos pontos */
+        if (token) m++; // Ao ler uma nova coordenada, atualiza a dimensão dos pontos
     }
 
     free(line_buf);
@@ -32,9 +32,9 @@ static int leM(FILE *fp, PlanoR *p) {
     return m;
 }
 
-static int leN(FILE *fp, PlanoR *p) {
+static int leN(FILE* fp, PlanoR* p) {
     //le o resto do arquivo e retorna n
-    char *line_buf = NULL;
+    char* line_buf = NULL;
     size_t line_buf_size = 0;
     int line_count = 0;
 
@@ -48,8 +48,8 @@ static int leN(FILE *fp, PlanoR *p) {
     return line_count;
 }
 
-PlanoR *leArquivo(char *FILENAMEINPUT, PlanoR *p) {
-    FILE *fp = fopen(FILENAMEINPUT, "r");
+PlanoR* leArquivo(char* FILENAMEINPUT, PlanoR* p) {
+    FILE* fp = fopen(FILENAMEINPUT, "r");
     if (!fp) {
         fprintf(stderr, "Erro ao abrir arquivo '%s'\n", FILENAMEINPUT);
         exit(1);
@@ -69,32 +69,32 @@ PlanoR *leArquivo(char *FILENAMEINPUT, PlanoR *p) {
         exit(1);
     }
 
-    /* Open the file for reading */
-    char *line_buf = NULL;
+    // Open the file for reading
+    char* line_buf = NULL;
     size_t line_buf_size = 0;
 
     initVetorPontos (p, n, m);
 
-    /* Loop through until we are done with the file. */
+    // Loop through until we are done with the file.
     for (int i = 0; i < n; ++i) {
         getline(&line_buf, &line_buf_size, fp);
 
-        /* Show the line details */
+        // Show the line details
         // printf("line[%06d]: contents: %s", line_count, line_buf);
-        char *token = strtok(line_buf, ",");
-        char *idPonto = strdup(token);
+        char* token = strtok(line_buf, ",");
+        char* idPonto = strdup(token);
 
-        double *coords = (double *) malloc(sizeof(double) * m);
+        double* coords = (double* ) malloc(sizeof(double)*  m);
         for (int j = 0; j < m; ++j) {
             token = strtok(NULL, ",");
             coords[j] = strtod(token, NULL);
         }
 
-        Ponto *novoPonto = initPonto(idPonto, coords, i);
+        Ponto* novoPonto = initPonto(idPonto, coords, i);
         insere(p, novoPonto, i);
     }
 
-    /* Free the allocated line buffer */
+    // Free the allocated line buffer
     free(line_buf);
     line_buf = NULL;
 
