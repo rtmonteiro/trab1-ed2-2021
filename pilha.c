f62dc7295cc1d2a3dd54664777e5c8d75641f853
bea <beamaia.work@gmail.com>
2021-03-09 12:05:53 -0300

    comenta conteudo de pilha

--- pilha.c
+++ pilha.c
@@ -1,113 +1,113 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "pilha.h"
-#include "ponto.h"
-#include "distancia.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "pilha.h"
+// #include "ponto.h"
+// #include "distancia.h"
 
-typedef struct celula Cel;
-struct celula {
-    Ponto* ponto;
-    Cel *prox;
-};
+// typedef struct celula Cel;
+// struct celula {
+//     Ponto* ponto;
+//     Cel *prox;
+// };
 
-struct pilha {
-    int qtd; // quantidade de pontos na pilha
-    int dimensao; // dimensao das coordenadas do ponto
-    Cel* pontos; 
-};
-
-
-Pilha* initPilha() {
-    Pilha* pilha = (Pilha*) malloc (sizeof(Pilha));
-    pilha->qtd = pilha->dimensao = 0;
-    pilha->pontos = NULL;
-    return pilha;
-}
+// struct pilha {
+//     int qtd; // quantidade de pontos na pilha
+//     int dimensao; // dimensao das coordenadas do ponto
+//     Cel* pontos; 
+// };
 
 
-void push(Pilha* pilha, Ponto* ponto) {
-    if (pilha == NULL || ponto == NULL) {
-        printf("Pilha ou ponto nao inicializado\n");
-        exit(1);
-    }
+// Pilha* initPilha() {
+//     Pilha* pilha = (Pilha*) malloc (sizeof(Pilha));
+//     pilha->qtd = pilha->dimensao = 0;
+//     pilha->pontos = NULL;
+//     return pilha;
+// }
+
+
+// void push(Pilha* pilha, Ponto* ponto) {
+//     if (pilha == NULL || ponto == NULL) {
+//         printf("Pilha ou ponto nao inicializado\n");
+//         exit(1);
+//     }
     
-    Cel* aux = (Cel*) malloc (sizeof(Cel));
-    aux->ponto = ponto;
+//     Cel* aux = (Cel*) malloc (sizeof(Cel));
+//     aux->ponto = ponto;
 
-    if (pilha->qtd > 0) {
-        aux->prox = pilha->pontos;
-    } else {
-        aux->prox = NULL;
-    }
+//     if (pilha->qtd > 0) {
+//         aux->prox = pilha->pontos;
+//     } else {
+//         aux->prox = NULL;
+//     }
 
-    pilha->pontos = aux;
-    pilha->qtd++;
-}
-
-
-Ponto* pop(Pilha* pilha) {
-    if (pilha == NULL || pilha->qtd > 0) {
-        printf("Pilha NULL ou vazia\n");
-        return NULL;
-    } 
-
-    Cel* aux = pilha->pontos;
-    pilha->pontos = pilha->pontos->prox;
-    pilha->qtd--;
-    aux->prox = NULL;
-
-    return aux->ponto;
-}
+//     pilha->pontos = aux;
+//     pilha->qtd++;
+// }
 
 
-Distancia** distanciasPilha (Pilha* pilha) {
-    Distancia** dist = initVetDistancia(pilha->qtd);
-    int i = 0;
+// Ponto* pop(Pilha* pilha) {
+//     if (pilha == NULL || pilha->qtd > 0) {
+//         printf("Pilha NULL ou vazia\n");
+//         return NULL;
+//     } 
 
-    // Percore pilha de forma que ponto1 é sempre diferente de ponto2 
-    // ponto2 eh o ponto apos ponto1
-    for(Cel* ponto1 = pilha->pontos; ponto1 != NULL ; ponto1=ponto1->prox) {
-        for (Cel* ponto2 = ponto1->prox; ponto2 != NULL; ponto2 = ponto2->prox, i++) {
-            // Calcula a distancia entre ponto1 e ponto2 e armazena num vetor
-            dist[i] = initDistancia(ponto1->ponto, ponto2->ponto, pilha->dimensao);
-        }
-    }
+//     Cel* aux = pilha->pontos;
+//     pilha->pontos = pilha->pontos->prox;
+//     pilha->qtd--;
+//     aux->prox = NULL;
 
-    ordenaVetDistancia(dist, pilha->qtd);
-
-    return dist;
-}
+//     return aux->ponto;
+// }
 
 
-int getQtd(Pilha* pilha) {
-    return pilha->qtd;
-}
+// Distancia** distanciasPilha (Pilha* pilha) {
+//     Distancia** dist = initVetDistancia(pilha->qtd);
+//     int i = 0;
+
+//     // Percore pilha de forma que ponto1 é sempre diferente de ponto2 
+//     // ponto2 eh o ponto apos ponto1
+//     for(Cel* ponto1 = pilha->pontos; ponto1 != NULL ; ponto1=ponto1->prox) {
+//         for (Cel* ponto2 = ponto1->prox; ponto2 != NULL; ponto2 = ponto2->prox, i++) {
+//             // Calcula a distancia entre ponto1 e ponto2 e armazena num vetor
+//             dist[i] = initDistancia(ponto1->ponto, ponto2->ponto, pilha->dimensao);
+//         }
+//     }
+
+//     ordenaVetDistancia(dist, pilha->qtd);
+
+//     return dist;
+// }
 
 
-int getDimensao(Pilha* pilha) {
-    return pilha->dimensao;
-}
+// int getQtd(Pilha* pilha) {
+//     return pilha->qtd;
+// }
 
 
-void setDimensao(Pilha* pilha, int m) {
-    pilha->dimensao = m;
-}
+// int getDimensao(Pilha* pilha) {
+//     return pilha->dimensao;
+// }
 
 
-static void liberaPontos (Cel* cel) {
-    Cel *aux1 = cel;
-    Cel *aux2;
-
-    while (aux1 != NULL) {
-        aux2 = aux1->prox;
-        liberaPonto(aux1->ponto);
-        free(aux1);
-        aux1 = aux2;
-    }
-}
+// void setDimensao(Pilha* pilha, int m) {
+//     pilha->dimensao = m;
+// }
 
 
-void liberaPilha (Pilha* pilha) {
-    liberaPontos(pilha->pontos);
-    free(pilha);
-}
+// static void liberaPontos (Cel* cel) {
+//     Cel *aux1 = cel;
+//     Cel *aux2;
+
+//     while (aux1 != NULL) {
+//         aux2 = aux1->prox;
+//         liberaPonto(aux1->ponto);
+//         free(aux1);
+//         aux1 = aux2;
+//     }
+// }
+
+
+// void liberaPilha (Pilha* pilha) {
+//     liberaPontos(pilha->pontos);
+//     free(pilha);
+// }
--- pilha.h
+++ pilha.h
@@ -1,84 +1,84 @@
-#ifndef PILHA_H
-    #define PILHA_H
+// #ifndef PILHA_H
+//     #define PILHA_H
 
-    #include "ponto.h"
-    #include "distancia.h"
+//     #include "ponto.h"
+//     #include "distancia.h"
 
 
-    // Define o tipo opaco Pilha
-    typedef struct pilha Pilha;
+//     // Define o tipo opaco Pilha
+//     typedef struct pilha Pilha;
 
-    /**
-     * Inicializa uma pilha vazia
-     * @return estrutura Pilha inicializada
-     * @pre nada
-     * @post pilha alocada
-    **/ 
-    Pilha* initPilha ();
+//     /**
+//      * Inicializa uma pilha vazia
+//      * @return estrutura Pilha inicializada
+//      * @pre nada
+//      * @post pilha alocada
+//     **/ 
+//     Pilha* initPilha ();
 
-    /**
-    * Coloca ponto como o ultimo elemento da pilha
-    * @param {Pilha*}pilha
-    * @param {Ponto*}ponto ponto que será inserido na pilha
-    * @return nada
-    * @pre pilha e ponto inicializados
-    * @post ponto é adicionado ao início da lista na pilha e o topo é incrementado 1
-    **/ 
-    void push (Pilha* pilha, Ponto* ponto);
+//     /**
+//     * Coloca ponto como o ultimo elemento da pilha
+//     * @param {Pilha*}pilha
+//     * @param {Ponto*}ponto ponto que será inserido na pilha
+//     * @return nada
+//     * @pre pilha e ponto inicializados
+//     * @post ponto é adicionado ao início da lista na pilha e o topo é incrementado 1
+//     **/ 
+//     void push (Pilha* pilha, Ponto* ponto);
 
-    /**
-    * Retira o ultimo elemento da pilha
-    * @param {Pilha*}pilha
-    * @return nada 
-    * @pre pilha inicializada
-    * @post ultimo ponto adicionado a lista é retirado
-    **/ 
-    Ponto* pop (Pilha* pilha);
+//     /**
+//     * Retira o ultimo elemento da pilha
+//     * @param {Pilha*}pilha
+//     * @return nada 
+//     * @pre pilha inicializada
+//     * @post ultimo ponto adicionado a lista é retirado
+//     **/ 
+//     Ponto* pop (Pilha* pilha);
 
-    /** 
-    * Percorre a pilha de forma que ponto1 != ponto2 e aplica a funcao func
-    * @param {Pilha*}pilha
-    * @return nada
-    * @pre pilha inicializada e não vazia
-    * @post vetor com distancias entre os pontos existe
-    **/ 
-    Distancia** distanciasPilha (Pilha* pilha);
+//     /** 
+//     * Percorre a pilha de forma que ponto1 != ponto2 e aplica a funcao func
+//     * @param {Pilha*}pilha
+//     * @return nada
+//     * @pre pilha inicializada e não vazia
+//     * @post vetor com distancias entre os pontos existe
+//     **/ 
+//     Distancia** distanciasPilha (Pilha* pilha);
 
-    /** 
-    * Retorna quantidade de elementos na pilha
-    * @param {Pilha*}pilha
-    * @return int quantidade de itens na pilha
-    * @pre pilha inicializada e não vazia
-    * @post nada
-    **/ 
-    int getQtd(Pilha* pilha);
+//     /** 
+//     * Retorna quantidade de elementos na pilha
+//     * @param {Pilha*}pilha
+//     * @return int quantidade de itens na pilha
+//     * @pre pilha inicializada e não vazia
+//     * @post nada
+//     **/ 
+//     int getQtd(Pilha* pilha);
 
-    /** 
-    * Retorna a dimensão de coordenadas dos pontos
-    * @param {Pilha*}pilha
-    * @return int dimensão dos pontos da pilha
-    * @pre pilha inicializada e não vazia
-    * @post nada
-    **/ 
-    int getDimensao(Pilha* pilha);
+//     /** 
+//     * Retorna a dimensão de coordenadas dos pontos
+//     * @param {Pilha*}pilha
+//     * @return int dimensão dos pontos da pilha
+//     * @pre pilha inicializada e não vazia
+//     * @post nada
+//     **/ 
+//     int getDimensao(Pilha* pilha);
 
-    /** 
-    * Modifica a dimensão de coordenadas dos pontos da pilha
-    * @param {Pilha*}pilha
-    * @param {int}m dimensão dos ponto
-    * @return int dimensão dos pontos da pilha
-    * @pre pilha inicializada e não vazia
-    * @post dimensao modificada
-    **/ 
-    void setDimensao(Pilha* pilha, int m);
+//     /** 
+//     * Modifica a dimensão de coordenadas dos pontos da pilha
+//     * @param {Pilha*}pilha
+//     * @param {int}m dimensão dos ponto
+//     * @return int dimensão dos pontos da pilha
+//     * @pre pilha inicializada e não vazia
+//     * @post dimensao modificada
+//     **/ 
+//     void setDimensao(Pilha* pilha, int m);
     
-    /** 
-    * Libera memoria alocada pela pilha
-    * @param {Pilha*}pilha
-    * @return nada
-    * @pre pilha inicializada 
-    * @post memoria alocada para pontos da pilha e a pilha eh liberada
-    **/ 
-    void liberaPilha (Pilha* pilha);
+//     /** 
+//     * Libera memoria alocada pela pilha
+//     * @param {Pilha*}pilha
+//     * @return nada
+//     * @pre pilha inicializada 
+//     * @post memoria alocada para pontos da pilha e a pilha eh liberada
+//     **/ 
+//     void liberaPilha (Pilha* pilha);
 
-#endif
+// #endif
