69cca6d16dae26ecbb48706c71a027ed6e3733af
bea <beamaia.work@gmail.com>
2021-03-09 18:11:17 -0300

    modifica parametro de ordenaVetDistancia

--- planoR.c
+++ planoR.c
@@ -31,10 +31,9 @@ void insere (PlanoR* plano, Ponto* ponto, int i) {
 Distancia** distanciasPontos (PlanoR* plano) {
     Distancia** dist = initVetDistancia(plano->qtd);
     int i = 0;
-
-    // Percore pilha de forma que ponto1 é sempre diferente de ponto2 
+    // Percore pilha de forma que ponto1 é sempre diferente de ponto2
     // ponto2 eh o ponto apos ponto1
-    for(int ponto1 = 0; i < plano->qtd; ponto1++) {
+    for(int ponto1 = 0; ponto1 < plano->qtd; ponto1++) {
         for (int ponto2 = ponto1 + 1; ponto2 < plano->qtd; ponto2++, i++) {
             // Calcula a distancia entre ponto1 e ponto2 e armazena num vetor
             dist[i] = initDistancia(plano->pontos[ponto1], plano->pontos[ponto2], plano->dim);
@@ -46,7 +45,7 @@ Distancia** distanciasPontos (PlanoR* plano) {
     return dist;
 }
 
-Ponto* getPontos(PlanoR* plano) {
+Ponto** getPontos(PlanoR* plano) {
     return plano->pontos;
 }
 
