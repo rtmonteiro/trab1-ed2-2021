5c2309d8e4f35acca5187b2126cdbc67db4c1eea
bea <beamaia.work@gmail.com>
2021-03-12 20:54:41 -0300

    faz retornar pilha

--- leArquivo.c
+++ leArquivo.c
@@ -7,7 +7,7 @@
 #define TAM 10
 
 
-static int leM(FILE* fp, PlanoR* p) {
+static int leM(FILE* fp) {
 
     //le a primeira linha e retorna m
     char* line_buf = NULL;
@@ -29,31 +29,82 @@ static int leM(FILE* fp, PlanoR* p) {
     return m;
 }
 
-static int leN(FILE* fp, PlanoR* p) {
-    //le o resto do arquivo e retorna n
-    char* line_buf = NULL;
-    size_t line_buf_size = 0;
-    int line_count = 0;
+// static int leN(FILE* fp) {
+//     //le o resto do arquivo e retorna n
+//     char* line_buf = NULL;
+//     size_t line_buf_size = 0;
+//     int line_count = 0;
 
-    while (!feof(fp)) {
-        line_count++;
-        getline(&line_buf, &line_buf_size, fp);
-    }
+//     while (!feof(fp)) {
+//         line_count++;
+//         getline(&line_buf, &line_buf_size, fp);
+//     }
 
-    free(line_buf);
+//     free(line_buf);
 
-    return line_count;
-}
+//     return line_count;
+// }
 
-PlanoR* leArquivo(char* FILENAMEINPUT, PlanoR* p) {
+// PlanoR* leArquivo(char* FILENAMEINPUT, PlanoR* p) {
+//     FILE* fp = fopen(FILENAMEINPUT, "r");
+//     if (!fp) {
+//         fprintf(stderr, "Erro ao abrir arquivo '%s'\n", FILENAMEINPUT);
+//         exit(1);
+//     }
+
+//     int m = leM(fp, p);
+//     int n = leN(fp, p);
+
+//     fclose(fp);
+
+//     //le o arquivo de novo e retorna o vetor com os pontos
+
+//     fp = fopen(FILENAMEINPUT, "r");
+//     if (!fp) {
+//         fprintf(stderr, "Erro ao abrir arquivo '%s'\n", FILENAMEINPUT);
+//         exit(1);
+//     }
+
+
+//     char* line_buf = NULL;
+//     size_t line_buf_size = 0;
+
+//     initVetorPontos (p, n, m);
+
+
+//     for (int i = 0; i < n; ++i) {
+//         getline(&line_buf, &line_buf_size, fp);
+
+//         char* token = strtok(line_buf, ",");
+//         char* idPonto = strdup(token);
+
+//         double* coords = (double* ) malloc(sizeof(double)*  m);
+//         for (int j = 0; j < m; ++j) {
+//             token = strtok(NULL, ",");
+//             coords[j] = strtod(token, NULL);
+//         }
+
+//         Ponto* novoPonto = initPonto(idPonto, coords, i);
+//         insere(p, novoPonto, i);
+//     }
+
+//     free(line_buf);
+//     line_buf = NULL;
+
+//     fclose(fp);
+
+//     return p;
+// }
+
+
+Pilha *leArquivo(char* FILENAMEINPUT, Pilha *p) {
     FILE* fp = fopen(FILENAMEINPUT, "r");
     if (!fp) {
         fprintf(stderr, "Erro ao abrir arquivo '%s'\n", FILENAMEINPUT);
         exit(1);
     }
 
-    int m = leM(fp, p);
-    int n = leN(fp, p);
+    int m = leM(fp);
 
     fclose(fp);
 
@@ -65,33 +116,40 @@ PlanoR* leArquivo(char* FILENAMEINPUT, PlanoR* p) {
         exit(1);
     }
 
-
+    setDimensaoPilha(p, m);
+//     //le o resto do arquivo e retorna n
     char* line_buf = NULL;
     size_t line_buf_size = 0;
+    int line_count = 0;
 
-    initVetorPontos (p, n, m);
+    getline(&line_buf, &line_buf_size, fp);
 
+    /* Loop through until we are done with the file. */
+    while (!feof(fp)) {
+        /* Increment our line count */
+        line_count++;
 
-    for (int i = 0; i < n; ++i) {
-        getline(&line_buf, &line_buf_size, fp);
-
-        char* token = strtok(line_buf, ",");
+        /* Show the line details */
+        // printf("line[%06d]: contents: %s", line_count, line_buf);
+        char *token = strtok(line_buf, ",");
         char* idPonto = strdup(token);
 
-        double* coords = (double* ) malloc(sizeof(double)*  m);
-        for (int j = 0; j < m; ++j) {
+        double* coords = (double*) malloc(sizeof(double) * m);
+        for (int i = 0; i < m; ++i) {
             token = strtok(NULL, ",");
-            coords[j] = strtod(token, NULL);
+            coords[i] = strtod(token, NULL);
         }
 
-        Ponto* novoPonto = initPonto(idPonto, coords, i);
-        insere(p, novoPonto, i);
+        Ponto* novoPonto = initPonto(idPonto, coords, line_count-1);
+        push(p, novoPonto);
+
+        /* Get the next line */
+        getline(&line_buf, &line_buf_size, fp);
     }
 
+    /* Free the allocated line buffer */
     free(line_buf);
     line_buf = NULL;
-
-    fclose(fp);
 
     return p;
 }
