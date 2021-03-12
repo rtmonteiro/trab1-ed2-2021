be508de1ae2c2edb2840ec95916e917f44e18a22
bea <beamaia.work@gmail.com>
2021-03-12 18:52:55 -0300

    tira pontos repetidos e corrige bug

--- in-exemplos/gerador_de_saida.py
+++ in-exemplos/gerador_de_saida.py
@@ -13,45 +13,40 @@ letras = string.hexdigits
 for i in range(N):
     ponto = []
     ponto.append("".join(random.choice(letras) for i in range(random.randint(1, 31))))
-    print("Ponto antes",ponto)
 
     tem_repetido = True
     while tem_repetido:
         tem_repetido = False
         coordenadas = []
 
-        
         for i in range(M):
             coordenadas.append(random.random() * 10)
         
-        for i, ponto in enumerate(pontos):
+        for i, ponto_obs in enumerate(pontos):
             if i == 0: 
                 continue
 
             iguais = 0
             for coord in coordenadas:
-                tem_igual = all(element == coord for element in ponto)
+                tem_igual = all(elemento == coord for elemento in ponto_obs)
                 
                 if tem_igual:
                     iguais += 1
-                
+             
             if iguais == len(coordenadas):
                 tem_repetido = True
-
-        print(tem_repetido)
+            else:
+                tem_igual = all(elemento == ponto[0] for elemento in ponto_obs)
+  
         if tem_repetido:
             continue
         else:
             break    
-
+    
     ponto.append(coordenadas)
-    print(*coordenadas)
-    print("Ponto depois:",ponto)
     pontos.append(ponto)
-    print()
 
 f = open(saida, "w")
-print(pontos)
 for ponto in pontos:
     f.write(ponto[0])
     for coord in ponto[1]:
