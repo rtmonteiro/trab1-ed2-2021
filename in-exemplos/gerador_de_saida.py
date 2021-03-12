9c27debf8ba75876817606092335d029977f1c17
bea <beamaia.work@gmail.com>
2021-03-12 18:33:49 -0300

    WIP: verifica se ponto já existe

--- in-exemplos/gerador_de_saida.py
+++ in-exemplos/gerador_de_saida.py
@@ -13,17 +13,49 @@ letras = string.hexdigits
 for i in range(N):
     ponto = []
     ponto.append("".join(random.choice(letras) for i in range(10)))
-    for i in range(M):
-        ponto.append(random.random() * 10)
+    print("Ponto antes",ponto)
+
+    tem_repetido = True
+    while tem_repetido:
+        tem_repetido = False
+        coordenadas = []
+
+        for i in range(M):
+            coordenadas.append(random.random() * 10)
+        
+        for i, ponto in enumerate(pontos):
+            if i == 0: 
+                continue
+
+            iguais = 0
+            for coord in coordenadas:
+                tem_igual = all(element == coord for element in ponto)
+                
+                if tem_igual:
+                    iguais += 1
+                
+            if iguais == len(coordenadas):
+                tem_repetido = True
+
+        print(tem_repetido)
+        if tem_repetido:
+            continue
+        else:
+            break    
+
+    ponto.append(coordenadas)
+    print(*coordenadas)
+    print("Ponto depois:",ponto)
     pontos.append(ponto)
+    print()
 
 f = open(saida, "w")
-
+print(pontos)
 for ponto in pontos:
     f.write(ponto[0])
-    for i in range(M):
+    for coord in ponto[1]:
         f.write(",")
-        f.write(str(ponto[i+1]))
+        f.write(str(coord))
     f.write("\n")
 
 # printing punctuation
