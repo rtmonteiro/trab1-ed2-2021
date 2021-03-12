22da1c0baa361acaab8f0cb5d47d2e88dd3b8ae4
bea <beamaia.work@gmail.com>
2021-03-12 17:59:30 -0300

    cria gerador de saidas

---
+++ in-exemplos/gerador_de_saida.py
@@ -0,0 +1,32 @@
+import string
+import random
+import sys
+
+args = sys.argv
+N = int(args[1])
+M = int(args[2])
+saida = args[3]
+
+pontos = []
+letras = string.hexdigits
+
+for i in range(N):
+    ponto = []
+    ponto.append("".join(random.choice(letras) for i in range(10)))
+    print(ponto)
+    for i in range(M):
+        ponto.append(random.random() * 10)
+    pontos.append(ponto)
+
+print(*pontos, sep="\n")
+
+f = open(saida, "w")
+
+for ponto in pontos:
+    f.write(ponto[0])
+    for i in range(M):
+        f.write(",")
+        f.write(str(ponto[i+1]))
+    f.write("\n")
+    
+# printing punctuation
