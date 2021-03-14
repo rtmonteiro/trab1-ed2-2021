046971903a0aa0ea50d1322d25bdfabf03ac10f7
bea <beamaia.work@gmail.com>
2021-03-13 22:04:26 -0300

    retirando N e M

--- main.c
+++ main.c
@@ -13,15 +13,12 @@ int main(int argc, char** argv ) {
 
     char *FILENAMEINPUT = argv[1];
     char *FILENAMEOUTPUT = argv[3];
-
     int k = (int) strtod(argv[2], NULL);
     
     Pilha* pilha = initPilha();
     pilha = leArquivo(FILENAMEINPUT, pilha);
 
-    int m = getDimensaoPilha(pilha);
-    int n = getQtdPilha(pilha);
-    PlanoR *plano = initPlanoR(n, m);
+    PlanoR *plano = initPlanoR(getQtdPilha(pilha), getDimensaoPilha(pilha));
     pilhaParaPlanoR(pilha, plano);
 
     Distancia **vetorDistancias = distanciasPontos(plano);
@@ -30,18 +27,14 @@ int main(int argc, char** argv ) {
     int tamDist = tam * (tam - 1) / 2;
 
     UF* unionFind = initUnionFind(getPontos(plano), tam);
-    // mostraUnionFind(unionFind);
     agrupaCaminhos(unionFind, vetorDistancias, tam, tamDist, k);
-    // // mostraUnionFind(unionFind);
 
     imprimeAgrupamentos(unionFind, FILENAMEOUTPUT);
 
-    //region Libera
     liberaPilha(pilha);
     liberaPlano(plano);
     liberaDistancia(vetorDistancias, tam);
     liberaUnionFind(unionFind);
-    //endregion
 
     return EXIT_SUCCESS;
 }
